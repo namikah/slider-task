6c2ea01445d06a8fd9b812a412fa1fcfe092b98b
namikah <namikah@code.edu.az>
2021-12-16 02:59:14 +0400

    ..

--- js/index.js
+++ js/index.js
@@ -4,29 +4,27 @@ let bigImage = document.querySelector(".popup .inner .slider-image img");
 let close = document.querySelector(".popup .inner .close");
 let rightArrow = document.querySelector(".arrows .right-arrow i");
 let leftArrow = document.querySelector(".arrows .left-arrow i")
+
 //image click for biggest
 images.forEach(element => {
-    element.addEventListener("click", (e) => {
+    element.addEventListener("click", function (e) {
+        resetClassList();
         e.preventDefault();
-        let imgSrc = e.target.getAttribute("src");
-        images.forEach(item => {
-            item.classList.remove("show-image")
-        });
-        openPopup(imgSrc);
-        element.classList.add("show-image");
-
-        //right-arrow click for change image
-        rightArrow.addEventListener("click", (e) => {
-            let curElement = document.querySelector(".show-image");
-            changeNext(curElement);
-        })
-         //left-arrow click for change image
-         leftArrow.addEventListener("click", (e) => {
-            let curElement = document.querySelector(".show-image");
-            changePrev(curElement);
-        })
+        openPopup(this);
+        this.classList.add("show-image");
     })
 });
+
+//right-arrow click for change image
+rightArrow.addEventListener("click", (e) => {
+    curElement = document.querySelector(".show-image");
+    changeNext(curElement);
+})
+//left-arrow click for change image
+leftArrow.addEventListener("click", (e) => {
+    curElement = document.querySelector(".show-image");
+    changePrev(curElement);
+})
 
 //close popup with X
 close.addEventListener("click", () => {
@@ -55,36 +53,45 @@ function closePopup() {
 
 //open popup function
 function openPopup(item) {
-    bigImage.setAttribute("src", item);
+    let imgSrc = item.getAttribute("href");
+    bigImage.setAttribute("src", imgSrc);
     popup.style.display = "flex";
 }
 
 //change images next function
-function changeNext(currentElement){
+function changeNext(currentElement) {
+    resetClassList();
     if (currentElement.nextElementSibling !== null) {
         currentElement.nextElementSibling.classList.add("show-image");
-        nextImageSrc = currentElement.nextElementSibling.getAttribute("href");
+        nextImageSrc = currentElement.nextElementSibling;
         openPopup(nextImageSrc);
     }
     else {
         currentElement.parentElement.children[0].classList.add("show-image");
-        openPopup(currentElement.parentElement.children[0].getAttribute("href"));
+        openPopup(currentElement.parentElement.children[0]);
     }
-    currentElement.classList.remove("show-image");
+    // currentElement.classList.remove("show-image");
 }
 
 //change images prev function
-function changePrev(currentElement){
+function changePrev(currentElement) {
+    resetClassList();
     let length = currentElement.parentElement.children.length;
-    
+
     if (currentElement.previousElementSibling !== null) {
         currentElement.previousElementSibling.classList.add("show-image");
-        nextImageSrc = currentElement.previousElementSibling.getAttribute("href");
+        nextImageSrc = currentElement.previousElementSibling;
         openPopup(nextImageSrc);
     }
     else {
-        currentElement.parentElement.children[length-1].classList.add("show-image");
-        openPopup(currentElement.parentElement.children[length-1].getAttribute("href"));
+        currentElement.parentElement.children[length - 1].classList.add("show-image");
+        openPopup(currentElement.parentElement.children[length - 1]);
     }
-    currentElement.classList.remove("show-image");
+    // currentElement.classList.remove("show-image");
+}
+
+function resetClassList() {
+    images.forEach(item => {
+        item.classList.remove("show-image")
+    });
 }
